c5ab2c7db4a332d1da58ed046d85f03c317b884d
Ben McKenzie <bmckenzie0522@gmail.com>
2020-07-20 11:00:51 -0400

    tests are passing now

--- anagrams.py
+++ anagrams.py
@@ -28,11 +28,13 @@ def find_anagrams(words):
     Example:
     {'dgo': ['dog'], 'act': ['cat', 'act']}
     """
-    anagrams = {
-        alphabetize(word): [
-            w for w in words
-            if alphabetize(w) == alphabetize(word)]
-        for word in words}
+    anagrams = {}
+    for word in words:
+        key = alphabetize(word)
+        if key in anagrams:
+            anagrams[key].append(word)
+        else:
+            anagrams[key] = [word]
     return anagrams
 
 
